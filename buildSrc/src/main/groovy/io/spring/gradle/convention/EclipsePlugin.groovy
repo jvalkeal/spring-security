aa4abad6c718f1cf08ddf64335ceefccc32fe297
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-03-22 17:00:40 +0000

    stash

--- buildSrc/src/main/groovy/io/spring/gradle/convention/EclipsePlugin.groovy
+++ buildSrc/src/main/groovy/io/spring/gradle/convention/EclipsePlugin.groovy
@@ -4,6 +4,12 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.plugins.ide.eclipse.EclipsePlugin
 
+/**
+ * Plugin to tweak .classpath settings so that eclipse/sts/vscode can
+ * be imported cleanly.
+ *
+ * @author Janne Valkealahti
+ */
 class EclipsePlugin implements Plugin<Project> {
 
 	@Override
@@ -14,6 +20,17 @@ class EclipsePlugin implements Plugin<Project> {
 			classpath {
 				file {
 					whenMerged {
+						// for aspects gradle eclipse integration wrongly creates lib entries for
+						// aspects/build/classes/java/main and aspects/build/resources/main which
+						// never has anything. eclipse/sts don't care, vscode language server
+						// complains about missing folders. So remove those from a .classpath
+						entries.removeAll {
+							entry -> entry.kind == 'lib' && (entry.path.contains('aspects/build/classes/java/main') || entry.path.contains('aspects/build/resources/main'))
+						}
+						// there are cycles and then dependencies between projects main sources and
+						// test sources. Looks like gradle eclipse integration is getting confused.
+						// thus just relax rules so that projects always sees everything from
+						// dependant project.
 						entries
 							.findAll { entry -> entry instanceof org.gradle.plugins.ide.eclipse.model.ProjectDependency }
 							.each {
